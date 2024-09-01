80a6fe2a523051ffbe06acd66c8e6ae548685dee
rizo0103 <muhammadrizo.shokirov2006@gmail.com>
2024-09-01 12:52:19 +0500

    first  h/w from course

--- js/script.js
+++ js/script.js
@@ -1,4 +1,24 @@
-"user strict";
+"use strict";
+// 1;
+const numberOfFilms = prompt("How many movies have you watch ?", '');
 
-console.log('B' + 'a' + +'n' + 'a');
- 
+// 2;
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false,
+};
+
+// 3;
+
+const q1 = prompt('One of the latest movies yoy`ve watched', '');
+const q2 = prompt('How would you rate it ?', '');
+const q3 = prompt('One of the latest movies yoy`ve watched', '');
+const q4 = prompt('How would you rate it ?', '');
+
+personalMovieDB.movies[`${q1}`] = q2;
+personalMovieDB.movies[`${q3}`] = q4;
+
+console.log(personalMovieDB);
