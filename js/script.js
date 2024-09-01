a5eb421ed367ab056212ed9c0fdfa9383da89002
rizo0103 <muhammadrizo.shokirov2006@gmail.com>
2024-09-01 13:00:44 +0500

    branch

--- js/script.js
+++ js/script.js
@@ -1,24 +1,18 @@
 "use strict";
-// 1;
-const numberOfFilms = prompt("How many movies have you watch ?", '');
 
-// 2;
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    private: false,
+const num = 50;
+
+switch (num) {
+    case 49:
+        console.log('Error');
+        break;
+    case 100:
+        console.log('Error');
+        break;
+    case 50:
+        console.log('Spot !');
+        break;
+    default:
+        console.log('Not this time :)');
+        break;
 };
-
-// 3;
-
-const q1 = prompt('One of the latest movies yoy`ve watched', '');
-const q2 = prompt('How would you rate it ?', '');
-const q3 = prompt('One of the latest movies yoy`ve watched', '');
-const q4 = prompt('How would you rate it ?', '');
-
-personalMovieDB.movies[`${q1}`] = q2;
-personalMovieDB.movies[`${q3}`] = q4;
-
-console.log(personalMovieDB);
