89882e9fdb0aab3947c83352c2026989d7d17e89
rizo0103rizo.sho <muhammadrizo.shokirov2006@gmail.com>
2024-09-02 16:42:57 +0500

    string work

--- js/script.js
+++ js/script.js
@@ -1,18 +1,25 @@
 "use strict";
 
-const num = 50;
-
-switch (num) {
-    case 49:
-        console.log('Error');
-        break;
-    case 100:
-        console.log('Error');
-        break;
-    case 50:
-        console.log('Spot !');
-        break;
-    default:
-        console.log('Not this time :)');
-        break;
+// 1;
+const sayHello = (name) => {
+    return `Привет, ${name}`;
 };
+
+// 2;
+const returnNeighboringNumbers = (number) => {
+    return [number - 1, number, number + 1];
+};
+
+// 3;
+const getMathResult = (a, b) => {
+    // if ((b <= 0 || typeof(b) !== 'number') || (a <= 0 || typeof(a) !== 'number')) {
+    //     return `${a}`;
+    // }
+    let str = a;
+    for (let i = 2; i <= b; ++i) {
+        str += `---${a * i}`;
+    }
+    return str;
+}
+
+console.log(getMathResult(10, 5));
