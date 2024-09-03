e4353c010b3fe884041c784ff46329855b91e6b8
rizo0103rizo.sho <muhammadrizo.shokirov2006@gmail.com>
2024-09-03 14:53:54 +0500

    no message kidding callback functions added

---
+++ js/callbackFunctions.js
@@ -0,0 +1,27 @@
+"use strict";
+
+function first() {
+    setTimeout(() => {
+        console.log(1);
+    }), 500;
+}
+
+function second() {
+    console.log(2);
+}
+
+// first();
+// second();
+
+function learnJS(lang, callback) {
+    setTimeout(() => {
+        console.log(`I'm learning ${lang}`);
+    }, 1500);
+    callback();
+}
+
+function done() {
+    console.log(`I've finished this lesson`);
+}
+
+learnJS('JavaScript', done);
