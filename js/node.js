60d560256a11d0813b090d007200a99c3c9630a9
rizo0103rizo.sho <muhammadrizo.shokirov2006@gmail.com>
2024-09-03 11:17:49 +0500

    work with functions

--- js/node.js
+++ js/node.js
@@ -1,1 +1,23 @@
+"use strict";
+
+// 1;
+function calculateVolumeAndArea(val) {
+    if (typeof(val) === 'number' && Math.round(val) === val && val > 0) {
+        return `Объем куба: ${val * val * val}, площадь всей поверхности: ${6 * val * val}`; 
+    } else {
+        return 'При вычислении произошла ошибка';
+    }
+}
+
+// 2;
+
+function getCoupeNumber(number) {
+    if (number === 0 || number > 36) {
+        return "Таких мест в вагоне не существует";
+    } else if (number < 0 || typeof(number) != 'number' || Math.round(number) != number) {
+        return "Ошибка. Проверьте правильность введенного номера места";
+    } else {
+        return Math.ceil(number / 4);
+    }
+}
 
