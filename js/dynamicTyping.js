55da79acc843f0d43bd7a4fbe64b8a87ad7e3dc0
rizo0103rizo.sho <muhammadrizo.shokirov2006@gmail.com>
2024-09-04 14:06:55 +0500

    learning dynamic typing in JS

---
+++ js/dynamicTyping.js
@@ -0,0 +1,52 @@
+"use strict";
+
+// To string
+
+// 1;
+console.log(typeof(String(null)));
+console.log(typeof(String(4)));
+
+// 2;
+console.log(typeof(null + ''));
+
+const num = 5;
+
+console.log(`https://vk.com/catalog/` + num);
+
+const fontSize = 26 + 'px';
+
+// To number
+
+// 1;
+console.log(typeof(Number('4')));
+
+// 2;
+console.log(typeof(+'5'));
+
+// 3;
+console.log(typeof(parseInt("15px", 10)));
+
+let answer = +prompt('Hello', '');
+
+// To boolean
+
+// 1;
+// 0, '', null, undefined, NaN;
+
+let switcher = null;
+
+if (switcher) {
+    console.log('Working...');
+}
+
+switcher = 1;
+
+if (switcher) {
+    console.log('Working...');
+}
+
+// 2;
+console.log(typeof(Boolean('4')));
+
+// 3;
+console.log(typeof(!!"4444"));
