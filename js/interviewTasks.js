c0bec5cc928810ede7e1cc2f9775baa587ffcee5
rizo0103rizo.sho <muhammadrizo.shokirov2006@gmail.com>
2024-09-04 15:20:56 +0500

    tasks from interview

---
+++ js/interviewTasks.js
@@ -0,0 +1,31 @@
+  "use strict";
+
+// let x = 5; 
+// console.log(x++); // 2
+
+// [] + false - null + trues
+// console.log([] + false - null + true); // "NaN"
+
+// let y = 1;
+// let x = y = 2;
+
+// console.log(x); // 2
+
+// console.log([] + 1 + 2); // "12"
+
+// console.log("1"[0]); // "1"
+
+// console.log(2 && 1 && null && 0 && undefined); // null
+
+// console.log(!!(1 && 2) === (1 && 2)); // false
+
+// console.log(null || 2 && 3 || 4); // 3
+
+// const a = [1, 2, 3];
+// const b = [1, 2, 3];
+
+// console.log(a == b); // false
+
+// console.log(+"Infinity") // Infinity
+
+// console.log('Ёжик' > 'Яблоко') // false
