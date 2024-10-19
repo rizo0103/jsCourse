df17f8788ad9517010f013979de8311d4049be88
rizo0103rizo.sho <muhammadrizo.shokirov2006@gmail.com>
2024-10-19 11:13:43 +0500

    learning recursion

--- js/styles.js
+++ js/styles.js
@@ -1,6 +1,13 @@
-"use strict"
+'use strict';
 
-const box = document.getElementById('box');
+const div = document.createElement('div');
+const btns = document.getElementsByTagName('button');
 
-box.style.display = 'flex';
-box.style.alignContent = 'center';
+div.classList.add('black');
+// div.style.width = '100px';
+// div.style.backgroundColor = 'black';
+
+// div.cssText = `width: 100%; height: 100%; background-color: black;`;
+
+// document.querySelector('.box').append(div);
+// btns[3].before(div);
