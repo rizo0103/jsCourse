632c5ea2c40323a25bbf18de462d77a20d534bea
rizo0103rizo.sho <muhammadrizo.shokirov2006@gmail.com>
2024-12-17 16:47:36 +0500

    learned set

---
+++ js/set/script.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const arr = ['Alex', 'Ann', 'Oleg', 'Alex'];
+
+function unique(arr) {
+    return Array.from(new Set(arr));
+}
+
+console.log(unique(arr));
+
+// const set = new Set(arr);
+
+// set.add('Ivan')
+//     .add('Oleg');
+
+// console.log(set);
+
+// set.delete(value);
+// set.has(value);
+// set.clear();
+// set.size;
+
+// for (let value of set) {
+//     console.log(value);
+// }
+
+// set.forEach((value, valueAgaing, set) => {
+//     console.log(value, valueAgaing);
+// });
+
+// console.log(set.values());
+// console.log(set.keys());
+// console.log(set.entries());
+
