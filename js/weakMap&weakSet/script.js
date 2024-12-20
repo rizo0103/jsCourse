dc969be3b58a0f98a6640d4af0b5b939320a5b30
rizo0103rizo.sho <muhammadrizo.shokirov2006@gmail.com>
2024-12-20 18:10:47 +0500

    learned how to work with eakSet

--- js/weakMap&weakSet/script.js
+++ js/weakMap&weakSet/script.js
@@ -1,9 +1,49 @@
 'use strict';
 
-let user = {name: 'Ivan'};
+// let user = {name: 'Ivan'};
 
-let map = new WeakMap();
-map.set(user, 'data');
-user = null;
+// let map = new WeakMap();
+// map.set(user, 'data');
 
-console.log(map);
+// user = null;
+
+// console.log(map);
+
+let cache = new WeakMap();
+
+function cacheUser(user) {
+    if (!cache.has(user)) {
+        cache.set(user, Date.now());
+    }
+
+    return cache.get(user);
+}
+
+let lena = {name: 'Elena'};
+let alex = {name: 'Alex'};
+
+cacheUser(lena);
+cacheUser(alex);
+
+lena = null;
+
+console.log(cache.has(lena));
+console.log(cache.has(alex));
+
+// WeakSet;
+// add, has, delete;
+
+let messages = [
+    { text: 'Hello', from: 'John' },
+    { text: 'Hello', from: 'Alex' },
+    { text: '....', from: 'M' },
+];
+
+let readMessages = new WeakSet();
+
+readMessages.add(messages[0]);
+// readMessages.add(messages[1]);
+
+readMessages.add(messages[0]);
+messages.shift();
+console.log(readMessages.has(messages[0]));
