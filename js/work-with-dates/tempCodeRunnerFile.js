e2155934ea3d516cfc2d6972605325e5cf4e9624
rizo0103rizo.sho <muhammadrizo.shokirov2006@gmail.com>
2024-12-21 10:23:04 +0500

    learned how to work with dates

---
+++ js/work-with-dates/tempCodeRunnerFile.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const now = new Date();
+
+console.log(now.setHours(18));
+console.log(now);
+
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getDate());
+// console.log(now.getDay());
+// console.log(now.getUTCHours());
+
+// console.log(now.getTimezoneOffset());
+// console.log(now.getTime());
