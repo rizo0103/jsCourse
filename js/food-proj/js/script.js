e1f52a0f8cab5b42cf3dace62a381c15c2779a68
rizo0103rizo.sho <muhammadrizo.shokirov2006@gmail.com>
2024-12-23 19:10:06 +0500

    created modal window

--- js/food-proj/js/script.js
+++ js/food-proj/js/script.js
@@ -102,4 +102,43 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     setClock('.timer', deadline);
+
+    // Modal
+
+    const modal = document.querySelector('.modal'),
+          btnModalOpen = document.querySelectorAll('[data-modal]'),
+          btnModalClose = document.querySelector('[data-close]');
+
+    for (let btns of btnModalOpen) {
+        btns.addEventListener('click', () => {
+            modal.classList.add('show');
+            modal.classList.remove('hide');
+            // modal.classList.toggle('show');
+            document.body.style.overflow = 'hidden';
+        });
+    }
+    
+    // btnModalOpen.addEventListener('click', () => {
+    //     modal.classList.add('show');
+    // });
+
+    function closeModal() {
+        modal.classList.remove('show');
+        modal.classList.add('hide');
+        document.body.style.overflow = '';
+    }
+
+    btnModalClose.addEventListener('click', closeModal);
+
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            closeModal();
+        }
+    });
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape' && modal.classList.contains('show')) {
+            closeModal();
+        }
+    });
 });
