8d1816f139ab403996124edb0da33aa37990b674
rizo0103rizo.sho <muhammadrizo.shokirov2006@gmail.com>
2024-12-23 17:59:39 +0500

    created timer for site

--- js/food-proj/js/script.js
+++ js/food-proj/js/script.js
@@ -36,4 +36,65 @@ window.addEventListener('DOMContentLoaded', () => {
             });
         }
     });
+
+    // Timer;
+
+    const deadline = '2024-12-24';
+
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+        days = Math.floor(t / (1000 * 60 * 60 * 24)),
+        hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+        minutes = Math.floor((t / 1000 / 60) % 60),
+        seconds = Math.floor((t / 1000) % 60);
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds,
+        };
+    }
+
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+            // console.log("asdsadasd");
+
+        function updateClock() {
+
+            const t = getTimeRemaining(endtime);
+
+            
+            days.innerText = getZero(t.days);
+            hours.innerText = getZero(t.hours);
+            minutes.innerText = getZero(t.minutes);
+            seconds.innerText = getZero(t.seconds);
+            
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+
+                days.innerText = 0;
+                hours.innerText = 0;
+                minutes.innerText = 0;
+                seconds.innerText = 0;
+            }
+        }
+    }
+
+    setClock('.timer', deadline);
 });
