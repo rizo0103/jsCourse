da4d7b3922b3c1e9a9cbabeb7e90431fc534ba41
rizo0103rizo.sho <muhammadrizo.shokirov2006@gmail.com>
2025-01-11 13:36:59 +0500

    learned about rest operator

--- js/food-proj/js/script.js
+++ js/food-proj/js/script.js
@@ -160,13 +160,14 @@ window.addEventListener('DOMContentLoaded', () => {
     // using classes for the cards;
 
     class Card {
-        constructor(data) {
-            this.src = data.src,
-            this.title = data.title;
-            this.description = data.description;
-            this.price = data.price;
-            this.parent = document.querySelector(data.parentSelector);
-            this.alt = data.alt;
+        constructor(src, alt, title, description, price, parentSelector, ...classes) {
+            this.src = src,
+            this.alt = alt;
+            this.title = title;
+            this.description = description;
+            this.price = price;
+            this.parent = document.querySelector(parentSelector);
+            this.classes = classes;
             this.transfer = 27;
             this.changeToUAH();
         }
@@ -177,17 +178,23 @@ window.addEventListener('DOMContentLoaded', () => {
 
         render() {
             const element = document.createElement('div');
-            
+
+            if (this.classes.length === 0) {
+                this.element = 'menu__item';
+
+                element.classList.add(this.element);
+            } else {
+                this.classes.forEach(className => element.classList.add(className));
+            }
+
             element.innerHTML = `
-                <div class="menu__item">
-                    <img src=${this.src} alt=${this.alt}>
-                    <h3 class="menu__item-subtitle">Меню "Фитнес"</h3>
-                    <div class="menu__item-descr">${this.description}</div>
-                    <div class="menu__item-divider"></div>
-                    <div class="menu__item-price">
-                        <div class="menu__item-cost">Цена:</div>
-                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-                    </div>
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">Меню "Фитнес"</h3>
+                <div class="menu__item-descr">${this.description}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
                 </div>
             `;
 
@@ -195,31 +202,35 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     };
 
-    new Card({
-        src: "./img/tabs/vegy.jpg",
-        alt: "vegy",
-        title: 'Меню "Фитнес"',
-        description: 'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеств',
-        price: 9,
-        parentSelector: '.menu .container',
-    }).render();
+    new Card(
+        "./img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеств',
+        9,
+        '.menu .container',
+        // 'menu__item',
+        // 'big',
+    ).render();
 
-    new Card({
-        src: "./img/tabs/elite.jpg",
-        alt: "elite",
-        title: 'Меню “Премиум”',
-        description: 'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-        price: 14,
-        parentSelector: '.menu .container',
-    }).render();
+    new Card(
+        "./img/tabs/elite.jpg",
+        "elite",
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        14,
+        '.menu .container',
+        'menu__item',
+    ).render();
 
-    new Card({
-        src: "./img/tabs/post.jpg",
-        alt: "post",
-        title: 'Меню "Постное"',
-        description: 'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков. ',
-        price: 21,
-        parentSelector: '.menu .container',
-    }).render();
+    new Card(
+        "./img/tabs/post.jpg",
+        "post",
+        'Меню "Постное"',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков. ',
+        21,
+        '.menu .container',
+        'menu__item',
+    ).render();
 
 });
