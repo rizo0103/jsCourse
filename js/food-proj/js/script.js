b27877a3e1e2b0039572639db268935a37142de8
rizo0103rizo.sho <muhammadrizo.shokirov2006@gmail.com>
2025-01-11 11:31:17 +0500

    used classes for rendering cards

--- js/food-proj/js/script.js
+++ js/food-proj/js/script.js
@@ -146,7 +146,7 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    const modalTimerId = setTimeout(openModal, 5000);
+    // const modalTimerId = setTimeout(openModal, 5000);
 
     function showModalByScroll() {
         if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) {
@@ -156,4 +156,70 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     window.addEventListener('scroll', showModalByScroll);
+
+    // using classes for the cards;
+
+    class Card {
+        constructor(data) {
+            this.src = data.src,
+            this.title = data.title;
+            this.description = data.description;
+            this.price = data.price;
+            this.parent = document.querySelector(data.parentSelector);
+            this.alt = data.alt;
+            this.transfer = 27;
+            this.changeToUAH();
+        }
+
+        changeToUAH() {
+            this.price *= this.transfer;
+        }
+
+        render() {
+            const element = document.createElement('div');
+            
+            element.innerHTML = `
+                <div class="menu__item">
+                    <img src=${this.src} alt=${this.alt}>
+                    <h3 class="menu__item-subtitle">Меню "Фитнес"</h3>
+                    <div class="menu__item-descr">${this.description}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                    </div>
+                </div>
+            `;
+
+            this.parent.append(element);
+        }
+    };
+
+    new Card({
+        src: "./img/tabs/vegy.jpg",
+        alt: "vegy",
+        title: 'Меню "Фитнес"',
+        description: 'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеств',
+        price: 9,
+        parentSelector: '.menu .container',
+    }).render();
+
+    new Card({
+        src: "./img/tabs/elite.jpg",
+        alt: "elite",
+        title: 'Меню “Премиум”',
+        description: 'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        price: 14,
+        parentSelector: '.menu .container',
+    }).render();
+
+    new Card({
+        src: "./img/tabs/post.jpg",
+        alt: "post",
+        title: 'Меню "Постное"',
+        description: 'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков. ',
+        price: 21,
+        parentSelector: '.menu .container',
+    }).render();
+
 });
