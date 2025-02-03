6231ea9547b5833799372b4ff8ad43e24565df69
rizo0103rizo.sho <muhammadrizo.shokirov2006@gmail.com>
2025-02-03 14:22:11 +0500

    getters and setters in objects

---
+++ js/getters&setters/script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const persone = {
+    name: 'Alex',
+    age: 25,
+
+    get userAge() {
+        return this.age;
+    },
+
+    set userAge(num) {
+        this.age = num;
+    },
+};
+
+console.log(persone.userAge = 10);
